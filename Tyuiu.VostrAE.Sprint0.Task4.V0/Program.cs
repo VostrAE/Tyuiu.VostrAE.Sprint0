5832517718b2535ca024a7f3f10e9d68b7840870
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-11 16:47:40 +0500

    Task4

---
+++ Tyuiu.VostrAE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tyuiu.VostrAE.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1, 5));
+        Console.WriteLine(DataService.Subtraction(15, 5));
+        Console.WriteLine(DataService.Multiplication(10, 10));
+        Console.WriteLine(DataService.Division(5, 5));
+        Console.ReadKey();
+    }
+}
