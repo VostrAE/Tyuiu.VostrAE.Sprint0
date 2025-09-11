bc8b17709260b43d5efd2eb055897be68d3ac85e
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-11 19:42:51 +0500

    Task6

---
+++ Tyuiu.VostrAE.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tyuiu.VostrAE.Sprint0.Task6.V0.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+        Console.WriteLine("Разность элементов массива  = " + DataService.SubstractionArray(numsArray));
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+        Console.ReadKey();
+    }
+}
