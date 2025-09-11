6378d74b51959cca5c35d58e9d2fe6547fbf971c
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-11 20:58:02 +0500

    Task7

---
+++ Tyuiu.VostrAE.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VostrAE.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.VostrAE.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void CheckAdditionValid() {
+            int[] nums1 = new int[] { 1, 1, 1, 1, 1, };
+            int[] nums2 = new int[] { 1, 1, 1, 1, 1, };
+            int[] nums3 = new int[] { 2, 2, 2, 2, 2, };
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEquivalent(nums3, res);
+        } 
+    }
+}
