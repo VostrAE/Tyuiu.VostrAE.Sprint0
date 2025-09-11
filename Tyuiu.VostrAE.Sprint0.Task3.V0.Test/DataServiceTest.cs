210e1e0e5300ca8360b69062f1893a7975f6f2c4
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-11 15:32:25 +0500

    Task3

---
+++ Tyuiu.VostrAE.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VostrAE.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.VostrAE.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Tyuiu.VostrAE.Sprint0.Task3.V0.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    { 
+    Console.WriteLine(DataService.Sum(7, 7));
+    Console.ReadKey();
+        }
+}
